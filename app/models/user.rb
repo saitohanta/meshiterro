c80a77cd0128a2e05617c15d98f8a0c87ae9f660
EC2 Default User <ec2-user@ip-172-31-22-236.ap-northeast-1.compute.internal>
2023-04-15 07:06:42 +0000

    [Add] 4-15

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ApplicationRecord
              file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
              profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
            end
-           profile_image.variant(resize_to_limit: [width, heig]).processed
+           profile_image.variant(resize_to_limit: [width, height]).processed
          end
 end
